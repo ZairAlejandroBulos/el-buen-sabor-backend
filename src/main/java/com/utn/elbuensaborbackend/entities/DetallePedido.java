83f146f4794b439199c7978021277b4605465afc
SantyCastillo <santy.castillo.00@gmail.com>
2023-05-18 18:51:09 -0300

    Add mapeo Pedido

--- src/main/java/com/utn/elbuensaborbackend/entities/DetallePedido.java
+++ src/main/java/com/utn/elbuensaborbackend/entities/DetallePedido.java
@@ -1,7 +1,6 @@
 package com.utn.elbuensaborbackend.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,5 +9,18 @@ import lombok.Setter;
 @Entity
 @Table(name = "detalle_pedido")
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@AttributeOverride(name = "id", column = @Column(name = "id_detalle_pedido"))
 public class DetallePedido extends Base {
+
+    @Column(name = "cantidad")
+    private int cantidad;
+
+    @Column(name = "subtotal")
+    private double subTotal;
+
+    @ManyToOne
+    @JoinColumn(name = "pedido_id")
+    private Pedido pedido;
+
+
 }
--- src/main/java/com/utn/elbuensaborbackend/entities/Pedido.java
+++ src/main/java/com/utn/elbuensaborbackend/entities/Pedido.java
@@ -1,14 +1,42 @@
 package com.utn.elbuensaborbackend.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.Date;
+import java.util.List;
+
 @Entity
 @Table(name = "pedido")
 @Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@AttributeOverride(name = "id", column = @Column(name = "id_pedido"))
 public class Pedido extends Base {
+
+    @Column(name = "fecha")
+    @Temporal(TemporalType.TIME)
+    private Date fecha;
+
+    @Column(name = "hora_estimada_fin")
+    @Temporal(TemporalType.TIME)
+    private Date horaEstimadaFin;
+
+    @Column(name = "monto_descuento")
+    private double montoDescuento;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<EstadoPedido> estadoPedidos;
+
+    @ManyToOne
+    @JoinColumn(name = "tipo_entrega_pedido_id")
+    private TipoEntregaPedido tipoEntregaPedido;
+
+    @ManyToOne
+    @JoinColumn(name = "tipo_pago_pedido_id")
+    private TipoPagoPedido tipoPagoPedido;
+
+    @OneToMany(mappedBy = "pedido")
+    private List<DetallePedido> detallePedido;
 }
